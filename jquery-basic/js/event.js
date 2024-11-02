254588975700331615dd9bd4e3c6c65abec17f3b
ta562 <tomo092777tomo@gmail.com>
2024-11-03 04:33:24 +0900

    a

---
+++ jquery-basic/js/event.js
@@ -0,0 +1,22 @@
+$(function(){
+    $('div').on({
+        'click':()=>{
+            $('div').css('background-color','red');
+            $('div').text('click');
+        },
+        'dblclick':()=>{
+            $('div').css('background-color','green');
+            $('div').text('dbclick');
+
+        },
+        'mouseenter':()=>{
+            $('div').css('background-color','blue');
+            $('div').text('mouseenter');
+
+        },
+        'mouseout':()=>{
+            $('div').css('background-color','gray');
+            $('div').text('mouseout');
+        },
+    })
+})
