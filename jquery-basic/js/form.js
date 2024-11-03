089b95d811cf397fb8cd5c3844b652bf193b5428
ta562 <tomo092777tomo@gmail.com>
2024-11-03 14:49:26 +0900

    a

---
+++ jquery-basic/js/form.js
@@ -0,0 +1,10 @@
+$(function(){
+    $('#check').on('click',function(){
+        console.log('名前：'+$('[name="username"]').val())
+        console.log('性別：'+$('[name="gender"]:checked').val())
+        console.log('血液型：'+$('[name="blood"]').val())
+        $('[name="hobby"]:checked').each(function(){
+            console.log('趣味：'+$(this).val())
+        })
+    })
+})
